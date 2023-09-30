6288f38fe7fde9f0954fd915961d1dd6b5133c79
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-09-30 23:54:30 +0530

    cards added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,26 +7,28 @@ function Home() {
         <>
             <div className='container' style={{ backgroundColor: Config.theme.accentColor }}>
                 <h1 className='text-center' style={{ color: Config.theme.DangerColor }}>{Config.title}</h1>
-                <p className='text-center'>{Config.description}</p>
-
-                <h2>services</h2>
-                {
+                <p className='text-des'>{Config.description}</p>
+               
+               <div className='flex-container'>
+               {
                     Config.services.map((serviceData, index) => {
                         const { title, img, description, price } = serviceData
                         return (
+
                             <div className='card m-3' key={index}>
                                 <div className='card-body'>
                                     <h3 className='card-title' style={{ color: Config.theme.secondaryColor }}>{title}</h3>
-
-                                    <img src={img} height={200} width={300} className='p-2 rounded-2' />
-
+                                    <img src={img} className='sweet-img' />
                                     <p className='card-text'>{description}</p>
-                                    <p className='card-text'>Price: {price}</p>
+                                    <p className='card-text'><b>Price:</b> {price}</p>
                                 </div>
                             </div>
                         )
                     })
                 }
+
+               </div>
+               
             </div>
         </>
     )
